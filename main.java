5c0ea05f2523aa22afc9d28b0c043bd457ed0a5b
Manuel <mpagoada@gmail.com>
2023-10-02 16:08:04 -0700

    set up main file and class file

---
+++ main.java
@@ -0,0 +1,18 @@
+
+
+public class main{
+    public static void menu(){
+        //gotta create a class that can take 3 diffrent integers for the date
+        //there should be 3 diffrent methods 
+        //it should also make sure you are only take valid inputs
+
+        int day, month, year;
+
+        
+
+
+
+    }
+
+
+}
