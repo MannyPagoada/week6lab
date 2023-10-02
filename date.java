6f61c1ba8501e035f5803d981052d1beb5f3394b
Manuel <mpagoada@gmail.com>
2023-10-02 16:32:45 -0700

    starting to set up dating with info and checking month

--- date.java
+++ date.java
@@ -1,5 +1,6 @@
 public class date {
     private int day, month, year;
+    private String monthString;
 
 
     //first constructor
@@ -13,11 +14,50 @@ public class date {
         this.year=year;
     }
 
+    //first date print
+    public void print()){
+        System.out.println(month + "/" + day + "/" + year);
+    }
+
+    //second date print
+    public void print2(){
+        System.out.println(null);
+
+    }
+
+    //set up class to figure out month in string
+    public void setMonthString(int month){
+        //switch that goes through every day of the month
+        //honesetly i can optimize it
+        switch (month){
+            case 1: 
 
 
+            case 2:
+            
+            case 3:
 
+            case 4:
 
+            case 5:
 
+            case 6:
+            
+            case 7:
+
+            case 8: 
+            
+            case 9:
+
+            case 10:
+
+            case 11:
+
+            case 12:
+        }
+    }
+
+    //setters all set up
     public void setDay(int day){
         this.day=day;
     }
@@ -28,6 +68,7 @@ public class date {
         this.year=year;
     }
 
+    //getters all set up
     public int getDay(){return day;}
     public int getMonth(){return month;}
     public int getYear(){return year;}
