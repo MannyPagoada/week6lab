487ea7b440286f6c0f8782eaafbb17338f93c513
Manuel <mpagoada@gmail.com>
2023-10-02 16:14:43 -0700

    set up the two constructors

--- date.java
+++ date.java
@@ -1,6 +1,16 @@
 public class date {
     int day, month, year;
-    
 
-    
+
+    //first constructor
+    public date(){
+    }
+ 
+    //second constructor
+    public date(int day, int month, int year){
+        this.day=day;
+        this.month=month;
+        this.year=year;
+    }
+
 }
