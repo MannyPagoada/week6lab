6aa72dd75ed5e8e788c466a136abb2c44b74f6fd
Manuel <mpagoada@gmail.com>
2023-10-02 17:17:50 -0700

    fixed the user inputs, now implementing the object

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class main{
-    public static void main(){
+    public static void main(String[] args){
         //gotta create a class that can take 3 diffrent integers for the date
         //there should be 3 diffrent methods 
         //it should also make sure you are only take valid inputs
@@ -11,19 +11,25 @@ public class main{
         do {
             System.out.println("Please enter the month (1-12): ");
             month=scanner.nextInt();}
-            while (month <= 12 && month >=12);
+            while (month > 13 || month < 0);
         
         
         do {
             System.out.println("Please enter day (0-31): ");
             day=scanner.nextInt();}
-            while(day <= 31 && day >=1);
+            while(day > 32 || day < 0);
 
         System.out.println("Please enter the year: ");
         year=scanner.nextInt();
 
-        System.out.println(day+month+year);
+        System.out.println("You entered: " + month + "/" + day + "/" + year);
 
+
+
+
+
+        //good practice to close the scanner when done with the code
+        scanner.close();
     }
 
 
